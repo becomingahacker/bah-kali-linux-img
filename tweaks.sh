b4923b20ed8f87124d10e1927a20933910570871
Chris McCoy <cmm@cisco.com>
2024-06-26 12:56:27 -0400

    Remove old tweaks

--- tweaks.sh
+++ tweaks.sh
@@ -10,27 +10,5 @@ flock -w 120 /var/lib/apt/lists/lock -c 'echo waiting for lock'
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y beef-xss mtr
-
-# Install Docker
-apt-get install -y ca-certificates curl
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  bookworm stable" | \
-  tee /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Install gcloud SDK, including Kubernetes
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-echo \
-  "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt \
-  cloud-sdk main" | \
-  tee /etc/apt/sources.list.d/google-cloud-sdk.list
-apt-get update
-apt-get install -y google-cloud-cli google-cloud-cli-gke-gcloud-auth-plugin google-cloud-cli-kubectl-oidc kubectl
-
 chmod u+x /provision/websploit.sh
 /provision/websploit.sh
